fd840a81bb585ead63d9efe5c6569756ed2ccbed
Rafał <riderbrand@gmail.com>
2021-12-16 23:26:28 +0100

    Set up project

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import "./css/App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Netflix Clone in React</h1>
+      <span>
+        Author: <a href="https://github.com/blueversi">Blueversi</a>
+      </span>
+    </div>
+  );
+}
+
+export default App;
