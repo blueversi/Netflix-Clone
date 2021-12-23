5464b7140b41485a6e86e3e35daec902ccf79877
Rafał <riderbrand@gmail.com>
2021-12-23 23:23:50 +0100

    Add header styles to positon it and it's elements

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,13 +1,18 @@
 /** @jsxImportSource @emotion/react */
 import React, { FC } from 'react';
+import { header, logo, navigation, button } from './css/Header.style';
 
 const Header: FC = () => {
   return (
-    <>
-      <div>
-        <h1>Header</h1>
+    <div css={header}>
+      <div css={logo}>
+        <h1>Logo</h1>
       </div>
-    </>
+      <div css={navigation}>
+        <button css={button}>Polski</button>
+        <button css={button}>Zaloguj</button>
+      </div>
+    </div>
   );
 };
 
---
+++ src/layouts/css/Header.style.tsx
@@ -0,0 +1,67 @@
+import { css } from '@emotion/react';
+
+/*
+----------------------------
+----------------------------
+      MEDIA QUERIES
+----------------------------
+----------------------------
+*/
+const breakpoints = [320, 769, 1024, 1440];
+
+// mq => media query
+const mq = breakpoints.map((bp) => `@media (min-width: ${bp}px)`);
+
+/* 
+for copy paste
+  [mq[0]]: {
+    flexDirection: 'column',
+  },
+  [mq[1]]: {
+    flexDirection: 'row',
+  },
+  [mq[2]]: {
+    flexDirection: 'row',
+  },
+  [mq[3]]: {
+    flexDirection: 'row',
+  },
+*/
+/*
+----------------------------
+----------------------------
+      MEDIA QUERIES
+----------------------------
+----------------------------
+*/
+
+export const header = css({
+  marginTop: 0,
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'nowrap',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  width: '100%',
+  backgroundColor: 'orange',
+});
+
+export const logo = css({
+  width: '80%',
+  backgroundColor: 'green',
+});
+
+export const navigation = css({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-evenly',
+
+  width: '20%',
+  backgroundColor: 'yellow',
+});
+
+export const button = css({
+  color: '#ffffff',
+  padding: '15px 40px',
+  backgroundColor: '#e50914',
+});
